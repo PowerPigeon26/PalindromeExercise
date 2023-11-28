bc280470cb4ee78dcf3023d3ed9a8ead3a1f6a85
Maverick Grayer <maverick72032@gmail.com>
2023-11-27 21:06:05 -0500

    Assignment complete with bonus

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var charArray = word.ToCharArray().Reverse().ToArray();
+            
+            return (word.ToLower() == (new string(charArray)).ToLower());
+        }
+
+        public bool IsASuperPalindrome(string word)
+        {
+            string simpleString = new string(word.Where(x => !char.IsPunctuation(x)).Where(x => x != ' ').ToArray());
+            string reversedString = new string(simpleString.ToCharArray().Reverse().ToArray());
+
+            return (simpleString.ToLower() == reversedString.ToLower());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,36 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("mom", true)]
+        [InlineData("rotator", true)]
+        [InlineData("Maverick", false)]
+        [InlineData("Sociopath", false)]
+        [InlineData("fish", false)]
+        public void Test1(string word, bool expected)
         {
+            WordSmith instance = new WordSmith();
 
+            var actual = instance.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("A man, a plan, a canal, Panama!", true)]
+        [InlineData("Never odd or even.", true)]
+        [InlineData("Maverick", false)]
+        [InlineData("Ah, hello hello?", false)]
+        [InlineData("Ugh, a bug!", false)]
+        public void Test2(string word, bool expected)
+        {
+            WordSmith instance = new WordSmith();
+
+            var actual = instance.IsASuperPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
